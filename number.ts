77f58a08faeb3d4a1bee7b26d8b0d9a1396316b7
piny4man <development@pinya.dev>
2023-05-20 23:20:26 +0200

    Add number snippets

---
+++ number.ts
@@ -0,0 +1,23 @@
+Number.isFinite(15) // true
+
+Number.isInteger(4.3) // false
+
+Number.isNaN(9) // false
+
+Number.isSafeInteger(45) // true
+
+Number.parseFloat('3.14 pi number') // 3.14
+
+Number.parseInt('100 times') // 100
+
+(123456).toExponential() // 1.23456e+5
+
+(12.503).toFixed() // 12
+
+(164).toLocaleString("ar-DJ") // ١٦٤
+
+(163.4334).toPrecision(3) // 163
+
+(163).toString() // "163"
+
+(163).valueOf() // 163
